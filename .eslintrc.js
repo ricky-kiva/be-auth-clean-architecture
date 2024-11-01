cee236be8dc579f690a22d5d2b9b48158010e49b
ricky-kiva <ricky.kiva@gmail.com>
2024-11-01 19:26:38 +0700

    feat(lint): init. ESLint with AirBnB config.

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'airbnb-base/legacy',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+  }
+};
