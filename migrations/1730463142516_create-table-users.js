c36164d2115fbeadb68498baf256f6a03aa6e491
ricky-kiva <ricky.kiva@gmail.com>
2024-11-01 19:27:35 +0700

    feat(db): add migration for `users` & `auths.` table

---
+++ migrations/1730463142516_create-table-users.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(36)',
+      primaryKey: true
+    },
+    username: {
+      type: 'VARCHAR(30)',
+      notNull: true,
+      unique: true
+    },
+    password: {
+      type: 'VARCHAR(255)',
+      notNull: true
+    },
+    fullname: {
+      type: 'VARCHAR(100)',
+      notNull: true
+    }
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
