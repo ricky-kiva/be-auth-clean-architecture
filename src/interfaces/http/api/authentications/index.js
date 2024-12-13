f07248293a6ede1fac0806f1c2dc9624a84b5b82
ricky-kiva <ricky.kiva@gmail.com>
2024-12-13 23:21:40 +0700

    feat(api): add `authentications` API

---
+++ src/interfaces/http/api/authentications/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const routes = require('./routes');
+const AuthenticationsHandler = require('./handler');
+
+module.exports = {
+  name: 'authentications',
+  register: async (server, { container }) => {
+    const authenticationsHandler = new AuthenticationsHandler(container);
+    server.route(routes(authenticationsHandler));
+  }
+};
